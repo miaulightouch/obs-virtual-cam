747cc079e2419d19da837a19a3fada9692e4d830
miau <miau@example.com>
2023-07-24 13:42:02 +0800

    fix: cmake-format

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -14,9 +14,7 @@ find_path(DEPS_ROOT "obs-studio-${obs_version}" PATHS "${CMAKE_PREFIX_PATH}" REQ
 list(PREPEND CMAKE_MODULE_PATH "${DEPS_ROOT}/obs-studio-${obs_version}/cmake/finders")
 
 function(install_libarary target)
-  install(
-    TARGETS ${target}
-    DESTINATION data/obs-plugins/${CMAKE_PROJECT_NAME})
+  install(TARGETS ${target} DESTINATION data/obs-plugins/${CMAKE_PROJECT_NAME})
 
   install(
     FILES "$<TARGET_PDB_FILE:${target}>"
@@ -46,7 +44,9 @@ function(parse_guid name guid)
       string(REPLACE ";" "0" GUID_HEX ${GUID_VALS})
       string(REGEX MATCH "[0-9a-fA-F]+" GUID_ACTUAL_HEX ${GUID_HEX})
       if(GUID_ACTUAL_HEX STREQUAL GUID_HEX)
-        set(${name}_GUID "${guid}" PARENT_SCOPE)
+        set(${name}_GUID
+            "${guid}"
+            PARENT_SCOPE)
         list(GET GUID_VALS 0 GUID_VALS_DATA1)
         list(GET GUID_VALS 1 GUID_VALS_DATA2)
         list(GET GUID_VALS 2 GUID_VALS_DATA3)
@@ -62,9 +62,15 @@ function(parse_guid name guid)
            AND GUID_VALS_DATA3_LENGTH EQUAL 4
            AND GUID_VALS_DATA4_LENGTH EQUAL 4
            AND GUID_VALS_DATA5_LENGTH EQUAL 12)
-          set(${name}_GUID_VAL01 ${GUID_VALS_DATA1} PARENT_SCOPE)
-          set(${name}_GUID_VAL02 ${GUID_VALS_DATA2} PARENT_SCOPE)
-          set(${name}_GUID_VAL03 ${GUID_VALS_DATA3} PARENT_SCOPE)
+          set(${name}_GUID_VAL01
+              ${GUID_VALS_DATA1}
+              PARENT_SCOPE)
+          set(${name}_GUID_VAL02
+              ${GUID_VALS_DATA2}
+              PARENT_SCOPE)
+          set(${name}_GUID_VAL03
+              ${GUID_VALS_DATA3}
+              PARENT_SCOPE)
           string(SUBSTRING ${GUID_VALS_DATA4} 0 2 GUID_VAL04)
           string(SUBSTRING ${GUID_VALS_DATA4} 2 2 GUID_VAL05)
           string(SUBSTRING ${GUID_VALS_DATA5} 0 2 GUID_VAL06)
@@ -73,14 +79,30 @@ function(parse_guid name guid)
           string(SUBSTRING ${GUID_VALS_DATA5} 6 2 GUID_VAL09)
           string(SUBSTRING ${GUID_VALS_DATA5} 8 2 GUID_VAL10)
           string(SUBSTRING ${GUID_VALS_DATA5} 10 2 GUID_VAL11)
-          set(${name}_GUID_VAL04 ${GUID_VAL04} PARENT_SCOPE)
-          set(${name}_GUID_VAL05 ${GUID_VAL05} PARENT_SCOPE)
-          set(${name}_GUID_VAL06 ${GUID_VAL06} PARENT_SCOPE)
-          set(${name}_GUID_VAL07 ${GUID_VAL07} PARENT_SCOPE)
-          set(${name}_GUID_VAL08 ${GUID_VAL08} PARENT_SCOPE)
-          set(${name}_GUID_VAL09 ${GUID_VAL09} PARENT_SCOPE)
-          set(${name}_GUID_VAL10 ${GUID_VAL10} PARENT_SCOPE)
-          set(${name}_GUID_VAL11 ${GUID_VAL11} PARENT_SCOPE)
+          set(${name}_GUID_VAL04
+              ${GUID_VAL04}
+              PARENT_SCOPE)
+          set(${name}_GUID_VAL05
+              ${GUID_VAL05}
+              PARENT_SCOPE)
+          set(${name}_GUID_VAL06
+              ${GUID_VAL06}
+              PARENT_SCOPE)
+          set(${name}_GUID_VAL07
+              ${GUID_VAL07}
+              PARENT_SCOPE)
+          set(${name}_GUID_VAL08
+              ${GUID_VAL08}
+              PARENT_SCOPE)
+          set(${name}_GUID_VAL09
+              ${GUID_VAL09}
+              PARENT_SCOPE)
+          set(${name}_GUID_VAL10
+              ${GUID_VAL10}
+              PARENT_SCOPE)
+          set(${name}_GUID_VAL11
+              ${GUID_VAL11}
+              PARENT_SCOPE)
           set(INVALID_GUID OFF)
         endif()
       endif()
